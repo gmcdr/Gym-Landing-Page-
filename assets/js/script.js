e407a1889bcc6fb86f96538dae2441af31d8bf47
Gabriel_Reis <tharazo101@gmail.com>
2021-12-29 21:09:52 -0300

    Landing Page

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//TROGGLE MENU
+function toggleMenu() {
+    const toggleMenu = document.querySelector('.toggleMenu');
+    const navbar = document.querySelector('.navbar');
+    toggleMenu.classList.toggle('active')
+    navbar.classList.toggle('active')
+}
+
